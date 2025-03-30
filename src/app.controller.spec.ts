96970d957649d878452f78c69c73902fbe1655c7
aolamide <aboyejiolamide15@gmail.com>
2025-03-31 00:06:59 +0100

    chore: Scaffold codebase migration to NestJS

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('Welcome to Quizza API v2!"', () => {
+      expect(appController.getWelcome()).toBe('Welcome to Quizza API v2!');
+    });
+  });
+});
